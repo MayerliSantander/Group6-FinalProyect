e1d62c3d6de13c1b8d010c30bcab113e90ff0485
DMichelR <aa6132788@gmail.com>
2022-03-22 22:12:18 -0400

    movie and user for tests

--- FinalProyect/src/main/java/com/mycompany/finalproyect/Billboard.java
+++ FinalProyect/src/main/java/com/mycompany/finalproyect/Billboard.java
@@ -12,6 +12,11 @@ import java.util.List;
 public class Billboard {
     List<Movie> movies = new ArrayList();
     
+    public Billboard(){
+        Movie movieTest = new Movie("test", "synopsis test", 777, "languages test", "genre test", "schedule test", "quality test");
+        movies.add(movieTest);
+    }
+    
     public void showBillboard(){
         System.out.println("La cartelera es:");
         for(Movie movie : movies ){
